63d1119c39b2e264e20d487b7ac880202338140a
Eric Fiorani <efiorani305@gmail.com>
2018-05-30 15:58:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,11 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    doc = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
